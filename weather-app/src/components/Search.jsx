5d1bc4e6224574d6600221f5d9c1b637e7349b2d
KLeonGreen <114649634+KLeonGreen@users.noreply.github.com>
2022-12-02 12:42:16 +0100

    weather fetch

--- weather-app/src/components/Search.jsx
+++ weather-app/src/components/Search.jsx
@@ -3,8 +3,9 @@ import { useEffect } from "react";
 
 const Home = () => {
   const [inputSearch, setInputSearch] = useState("");
-  const [citylat, setCitylat] = useState("");
-  const [citylon, setCitylon] = useState("");
+  const [citylat, setCitylat] = useState(1);
+  const [citylon, setCitylon] = useState(1);
+  const [weather, setWeather] = useState(null);
 
   const getCities = async () => {
     let response = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${inputSearch}&limit=1&appid=436d5eb42d8b4ef7b72bd8d989db2fc1`);
@@ -21,8 +22,8 @@ const Home = () => {
     let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${citylat}&lon=${citylon}&appid=436d5eb42d8b4ef7b72bd8d989db2fc1`);
 
     const data = await response.json();
-
-    console.log(data);
+    setWeather(data);
+    // console.log(data);
   };
 
   useEffect(() => {
@@ -30,16 +31,19 @@ const Home = () => {
   }, [inputSearch]);
 
   return (
-    <div>
-      <input
-        type="text"
-        placeholder="Search here"
-        onChange={(e) => {
-          setInputSearch(e.target.value);
-          getCities();
-        }}
-      />
-    </div>
+    <>
+      <div>
+        <input
+          type="text"
+          placeholder="Search here"
+          onChange={(e) => {
+            setInputSearch(e.target.value);
+            getCities();
+          }}
+        />
+      </div>
+      <div>{weather && <h3>{weather.timezone}</h3>}</div>
+    </>
   );
 };
 
