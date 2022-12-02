f855ba729ae2800abd815e0c534dd21f832b35de
KLeonGreen <114649634+KLeonGreen@users.noreply.github.com>
2022-12-02 13:17:18 +0100

    commit 3

--- weather-app/src/components/Search.jsx
+++ weather-app/src/components/Search.jsx
@@ -5,17 +5,21 @@ const Home = () => {
   const [inputSearch, setInputSearch] = useState("");
   const [citylat, setCitylat] = useState(1);
   const [citylon, setCitylon] = useState(1);
+  const [city, setCity] = useState(null);
   const [weather, setWeather] = useState(null);
 
-  const getCities = async () => {
-    let response = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${inputSearch}&limit=1&appid=436d5eb42d8b4ef7b72bd8d989db2fc1`);
+  const getCities = async (e) => {
+    if (e.key === "Enter") {
+      let response = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${inputSearch}&limit=1&appid=436d5eb42d8b4ef7b72bd8d989db2fc1`);
 
-    const data = await response.json();
-    setCitylat(data[0].lat);
-    setCitylon(data[0].lon);
-    console.log(data);
-    console.log(citylat);
-    console.log(citylon);
+      const data = await response.json();
+      setCity(data);
+      setCitylat(data[0].lat);
+      setCitylon(data[0].lon);
+      console.log(data);
+      console.log(citylat);
+      console.log(citylon);
+    }
   };
 
   const getWeather = async () => {
@@ -38,11 +42,17 @@ const Home = () => {
           placeholder="Search here"
           onChange={(e) => {
             setInputSearch(e.target.value);
-            getCities();
           }}
+          onKeyUp={getCities}
         />
       </div>
-      <div>{weather && <h3>{weather.timezone}</h3>}</div>
+      <div>
+        {weather && city && (
+          <h3>
+            it is {Math.floor(weather.main.temp - 273.15)}°C in {city[0].name}
+          </h3>
+        )}
+      </div>
     </>
   );
 };
